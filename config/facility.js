9d0c34913315722c177e4e788bf54770ac575aac
programista-nautil <programista@nautil.pl>
2025-05-23 09:46:06 +0200

    initial commmit

---
+++ config/facility.js
@@ -0,0 +1,15 @@
+export const FACILITY = {
+	name: 'Basen Delfin',
+	//logo: require('../assets/logo.png'),
+	address: 'ul. Sportowa 12, Gdańsk',
+	scheduleIframeUrl: 'https://docs.google.com/...',
+	priceList: [
+		{ label: 'Bilet normalny', price: '12 zł' },
+		{ label: 'Bilet ulgowy', price: '8 zł' },
+		{ label: 'Karnet 10 wejść', price: '100 zł' },
+	],
+	contact: {
+		phone: '123 456 789',
+		email: 'kontakt@basendelfin.pl',
+	},
+}
